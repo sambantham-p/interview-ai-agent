e380c2a05f3e2512606ecbf7b370eeb8c6f59967
sambantham <sambantham0901@gmail.com>
2026-09-08 08:00:45 +0530

    backend infra

---
+++ backend/app/constants.py
@@ -0,0 +1,9 @@
+INTERVIEW_PHASES = [
+    "background_check",
+    "project_drill_down",
+    "technical_interview",
+    "coding_challenge",
+    "general_technical",
+    "career_motivation",
+    "candidate_questions",
+]
